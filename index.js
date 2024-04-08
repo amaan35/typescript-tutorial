40340ae87ebec5d7a6271038b911b9d625c09469
amaan35 <amaanlm35@gmail.com>
2024-04-09 03:09:49 +0530

    defining types in TS

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 var age = 30;
 console.log(age);
+// How to define types
+var marks = 90;
+var firstName;
+console.log(marks);
+firstName = "Amaan";
+console.log(firstName);
--- index.ts
+++ index.ts
@@ -1,2 +1,10 @@
 let age = 30
 console.log(age);
+
+// How to define types
+let marks: number = 90
+let firstName: string
+
+console.log(marks);
+firstName = "Amaan";
+console.log(firstName);
