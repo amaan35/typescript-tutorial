5d921f92080020a2951f9dc6354538da3d03130e
amaan35 <amaanlm35@gmail.com>
2024-04-09 03:33:41 +0530

    defining Array

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,15 @@
 "use strict";
-let age = 30;
-console.log(age);
-// How to define types
-let marks = 90;
-let firstName;
-console.log(marks);
-firstName = "Amaan";
-console.log(firstName);
+//arrays
+let names;
+names = ['Syed', 'Amaan', 'Ali'];
+let ages = [10, 20, 30];
+names.push('hello');
+ages.push(40);
+console.log(names);
+console.log(ages);
+//type inference with arrays
+let fruits = ['mango', 'apple', 'banana'];
+fruits.push('peaches');
+const f = fruits[2]; //f is string
+let things = [1, true, 'hello'];
+const t = things[2]; //t is string|boolean|number
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
-let age = 30
-console.log(age);
+//arrays
 
-// How to define types
-let marks: number = 90
-let firstName: string
+let names: string[]
+names = ['Syed','Amaan', 'Ali'];
+let ages: number[] = [10,20,30];
 
-console.log(marks);
-firstName = "Amaan";
-console.log(firstName);
+names.push('hello')
+ages.push(40)
+
+console.log(names);
+console.log(ages);
+
+//type inference with arrays
+
+let fruits = ['mango', 'apple', 'banana']
+fruits.push('peaches');
+const f = fruits[2];    //f is string
+let things = [1, true, 'hello']
+const t = things[2]     //t is string|boolean|number
